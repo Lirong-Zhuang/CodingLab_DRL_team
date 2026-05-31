0fbea718c00e8fec6d40870b97b9d86af442381d
lejla27 <116877607+lejla27@users.noreply.github.com>
2026-05-31 18:29:42 +0200

    In the lines 27, 28 and 29 I noted how I will check which features actually help the agent

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ from environment import Environment
 data_dir = ...  # TODO: specify relative path to data directory (e.g., './data', not './data/variant_0')
 variant = ...  # TODO: specify problem variant (0 for base variant, 1 for first extension, 2 for second extension)
 env = Environment(variant, data_dir)
+
 env.feature_mode = "all" # ToDo: train, then change to "env.feature_mode = "no_distance" " and train again
                         # then  change to "env.feature_mode = "no_urgency"" and train and then to "env.feature_mode = "no_capacity"" and train again
                         # Compare results in a table and see which features actually help the agent
