0a9e64a76540276bb7fcb5d8e674b16c01a65e6c
Lirong Zhuang <lirong.zhuang@tum.de>
2026-06-07 11:52:51 +0200

    test setup

--- test_policy.py
+++ test_policy.py
@@ -2,16 +2,32 @@
 
 
 from environment import Environment
+from environment_v2 import Environment_v2
+from environment_v3 import Environment_v3
+from environment_v4 import Environment_v4
+from environment_v5 import Environment_v5
+from environment_v6 import Environment_v6
+from environment_v7 import Environment_v7
+from environment_v8 import Environment_v8
+import torch
+import dqn
+import rainbow_dqn
 
 
 def test_policy(env):
     test_rew = 0.  # initialize reward tracking
 
+    # network
+    network = rainbow_dqn.DQN_v8(env)
+    network.q_network.load_state_dict(torch.load('./models/DQN_v8.5.4_variant_2.pt', map_location=network.device))
+    network.q_network.eval()
+    network.epsilon = 0
+
     for i in range(100):  # loop over 100 test episodes
         obs = env.reset('testing')  # get initial obs
 
         for j in range(200):  # loop over 200 steps per episode
-            act = ...  # TODO: get action for the obs from your trained policy
+            act = network.select_action(obs)  # TODO: get action for the obs from your trained policy
             rew, next_obs, _ = env.step(act)  # take one step in the environment
             test_rew += rew  # track rewards
             obs = next_obs  # continue from the new obs
@@ -23,8 +39,8 @@ def test_policy(env):
 
 if __name__ == '__main__':
 
-    data_dir = ...  # TODO: specify relative path to data directory (e.g., './data', not './data/variant_0')
-    variant = ...  # TODO: specify problem variant (0 for base variant, 1 for first extension, 2 for second extension)
-    env = Environment(variant=variant, data_dir=data_dir)  # initialize the environment
+    data_dir = './data' # TODO: specify relative path to data directory (e.g., './data', not './data/variant_0')
+    variant = 2  # TODO: specify problem variant (0 for base variant, 1 for first extension, 2 for second extension)
+    env = Environment_v5(variant=variant, data_dir=data_dir)  # initialize the environment
 
     test_policy(env)  # test the trained policy
