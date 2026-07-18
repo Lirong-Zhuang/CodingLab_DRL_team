597a57c60545a65bfd00231aa8fb05db487b990b
Lirong-Zhuang <zhuang.lirong@outlook.com>
2026-07-18 10:23:47 +0200

    new env18

--- environment_v18.py
+++ environment_v18.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import numpy as np
 
 from environment_v11 import Environment_v11
@@ -14,12 +16,49 @@ class Environment_v18(Environment_v11):
         self.env_name = "18."
 
         eligible_cells = set(self.eligible_cells)
+        self.shortest_path_distances = self._build_shortest_path_distances(
+            eligible_cells
+        )
         self.blocked_cells = [
             (row, col)
             for row in range(self.vertical_cell_count)
             for col in range(self.horizontal_cell_count)
             if (row, col) not in eligible_cells
         ]
+
+    def _build_shortest_path_distances(self, eligible_cells):
+        """Precompute obstacle-aware distances between traversable cells."""
+        distances = {}
+
+        for start in eligible_cells:
+            start_distances = {start: 0}
+            queue = deque([start])
+
+            while queue:
+                row, col = queue.popleft()
+                for neighbor in (
+                    (row - 1, col),
+                    (row, col + 1),
+                    (row + 1, col),
+                    (row, col - 1),
+                ):
+                    if (
+                        neighbor in eligible_cells
+                        and neighbor not in start_distances
+                    ):
+                        start_distances[neighbor] = (
+                            start_distances[(row, col)] + 1
+                        )
+                        queue.append(neighbor)
+
+            for destination, distance in start_distances.items():
+                distances[(start, destination)] = distance
+
+        return distances
+
+    def get_dist_from_coord(self, coord1, coord2):
+        """Return the true shortest-path distance around blocked cells."""
+        return self.shortest_path_distances.get((coord1, coord2), np.inf)
 
     # CNN observation shape: 8 x 5 x 5
     def get_obs(self):
